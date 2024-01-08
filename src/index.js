93d0597adddc1c824db5448238d468ccf6a692e6
Araknet325 <luis.ssuarez555@gmail.com>
2024-01-08 20:57:41 -0300

    Create routes for categories, mainly post

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 const express = require('express')
 const db = require("./db");
+const routesCategories = require("./routes/categories");
 
 const app = express()
+app.use(express.json());
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World...!')
 });
 
-app.get("/categories", (req, res) => {
-  db.query("SELECT * FROM categories", (erro, response)=>{
-    if(error ){
-      return res.status(500).json(erro);
-    }
-
-    return res.status(200).json(response.rows);
-  });
-});
+app.use("/categories", routesCategories);
 
 app.listen(port, () => {
   db.connect().then(() =>{
